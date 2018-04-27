632891f56e99f29987614f3f34e203b2b901e8e8
chenxue <melthaw@gmail.com>
2018-04-27 22:52:59 +0800

    fixed: sms resource declare

--- sms/history/src/main/java/in/clouthink/synergy/sms/history/SmsHistoryResourceConfiguration.java
+++ sms/history/src/main/java/in/clouthink/synergy/sms/history/SmsHistoryResourceConfiguration.java
@@ -1,8 +1,10 @@
 package in.clouthink.synergy.sms.history;
 
 import in.clouthink.synergy.rbac.annotation.EnableResource;
+import in.clouthink.synergy.rbac.annotation.Permission;
 import in.clouthink.synergy.rbac.annotation.Resource;
 import in.clouthink.synergy.rbac.annotation.Metadata;
+import in.clouthink.synergy.rbac.model.Action;
 import org.springframework.context.annotation.Configuration;
 
 
@@ -10,11 +12,12 @@ import org.springframework.context.annotation.Configuration;
  * @author dz
  */
 @Configuration
-@EnableResource(
-        value = {@Resource(code = "value:dashboard:sms",
+@EnableResource({
+        @Resource(code = "sms:retrieve",
                 name = "短信发送记录",
-//						  patterns = {"/api/smsHistories**", "/api/smsHistories/**"},
-//						  actions = {@Action(code = "retrieve", name = "查看")},
-                metadata = {@Metadata(key = "state", value = "dashboard.smsHistory.list")})})
+                permission = {
+                        @Permission(api = "/api/sms-histories**", action = {Action.GET})
+                })
+})
 public class SmsHistoryResourceConfiguration {
 }
